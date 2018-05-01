8dca2c2874a265e4b7d3a56c809c67f32b8f173e
xuhui <zhaoxuhui1993@qq.com>
2018-05-01 23:57:21 +0800

    Update toc.py

--- toc.py
+++ toc.py
@@ -65,43 +65,44 @@ def deleteRightBracket(string):
 
 def deleteAdd(string):
     if string.__contains__('+'):
-        right_bracket = string.find('+')
-        string = string[:right_bracket] + string[right_bracket + 1:]
+        add_plus = string.find('+')
+        string = string[:add_plus] + string[add_plus + 1:]
         return string
 
 
 def deleteAnd(string):
     if string.__contains__('&'):
-        right_bracket = string.find('&')
-        string = string[:right_bracket] + string[right_bracket + 1:]
+        And = string.find('&')
+        string = string[:And] + string[And + 1:]
         return string
 
 
 def replaceDunHao(string, replace):
     if string.__contains__('、'):
         dun = string.find('、')
+        # 顿号是中文字符，占4个字节，所以加3
         string = string[:dun] + replace + string[dun + 3:]
         return string
 
 
 def replaceSharp(string, replace):
     if string.__contains__('#'):
-        dun = string.find('#')
-        string = string[:dun] + replace + string[dun + 1:]
+        sharp = string.find('#')
+        string = string[:sharp] + replace + string[sharp + 1:]
         return string
 
 
 def replaceSpace(string, replace):
     if string.__contains__(' '):
-        dun = string.find(' ')
-        string = string[:dun] + replace + string[dun + 1:]
+        space = string.find(' ')
+        string = string[:space] + replace + string[space + 1:]
         return string
 
 
 def replaceColon(string, replace):
     if string.__contains__('：'):
-        dun = string.find('：')
-        string = string[:dun] + replace + string[dun + 3:]
+        colon = string.find('：')
+        string = string[:colon] + replace + string[colon + 3:]
         return string
 
 
@@ -109,42 +110,37 @@ def getTitle(raw_str):
     # 1.分离#与标题内容，并确定标题等级
     res = raw_str.split(' ')
     level = res[0].count('#')
+    # 重新拼接标题内容，解决包含标题中包含多个空格时获取的标题内容不全的问题
     title = ""
     for i in range(1, res.__len__()):
+        # 解决重新拼接标题时，标题内的空格被删掉的问题
         title = title + " " + res[i]
-    # print(title)
+    # 去除标题行首的空格
     title = title.lstrip()
+    # 去除标题行尾的换行
     title = title.strip('\n')
     title_for_show = title
 
     # 2.删除特殊字符
     while title.__contains__('.'):
         title = deleteDot(title)
-        # print(title)
     while title.__contains__('('):
         title = deleteLeftParentheses(title)
-        # print(title)
     while title.__contains__(')'):
         title = deleteRightParentheses(title)
-        # print(title)
     while title.__contains__('`'):
         title = deleteSingleQuoteMark(title)
-        # print(title)
     while title.__contains__('、'):
         title = deleteDunHao(title)
-        # print(title)
     while title.__contains__('“'):
         title = deleteLeftDoubleQuoteMark(title)
-        # print(title)
     while title.__contains__('”'):
         title = deleteRightDoubleQuoteMark(title)
-        # print(title)
     while title.__contains__('['):
         title = deleteLeftBracket(title)
         # print(title)
     while title.__contains__(']'):
         title = deleteRightBracket(title)
-        # print(title)
     while title.__contains__('+'):
         title = deleteAdd(title)
     while title.__contains__('&'):
@@ -155,7 +151,6 @@ def getTitle(raw_str):
     # 3.英文大写变小写
     res = title.lower()
 
-    # print(res)
     return level, title_for_show, res
 
 
@@ -171,15 +166,14 @@ def getLink(base, string):
     return base + "#" + string
 
 
-def getBase(year, month, day, filename):
-    part1 = "http://zhaoxuhui.top/blog"
-    res = part1 + "/" + year + "/" + month + "/" + day + "/" + filename + ".html"
+def getBase(year, month, day, filename, part):
+    res = part + "/" + year + "/" + month + "/" + day + "/" + filename + ".html"
     return res
 
 
 def splitInfo(file_path):
+    # 文件名有固定的格式，xxxx-xx-xx-xxxxx.md
     index = file_path.rfind('\\')
-    path = file_path[:index]
     filename = file_path[index + 1:]
     temp = filename.split('-')
     year = temp[0]
@@ -207,33 +201,40 @@ def generateTOC(level, content):
     return content
 
 
-b_path = "F:\\Blog\\zhaoxuhui.github.io\\_posts\\"
-files = []
-f_path = raw_input("Input path of markdown file:\n")
-files.append(f_path)
-for fname in files:
+def execFunction(input_path):
+    # 博客网址的共有部分，可以替换成你自己的
+    part = "http://zhaoxuhui.top/blog"
 
-    path = b_path + fname
+    # 判断手动输入还是自动传入
+    flag = raw_input("Auto insert?y/n\n")
+    if flag == "y":
+        path = input_path
+    else:
+        path = raw_input("Input path of file:\n")
 
+    # 利用decode函数解决文件名中含有中文字符的问题
     f = open(path.decode('utf8'), 'r')
     headers = []
     lines = []
     line = f.readline()
     lines.append(line)
     while line:
-        # print line
         line = f.readline()
         lines.append(line)
+        # 通过每一行中含有的井号数量计算，因为在博客中一般不采用一、二级标题(太大了)
+        # 不好看，所以认为如果一行之中包含有连续两个井号，就认为是标题
         if line.__contains__("##"):
             headers.append(line.decode('utf-8').encode('utf-8'))
     f.close()
+    # 如果没找到标题，程序退出
     if headers.__len__() == 0:
         print("No title.")
-        continue
+        exit()
 
-    year, month, day, name = splitInfo(path)
-    base = getBase(year, month, day, name)
-    print(base)
+    # 获取除去`_auto`后缀的正确的名字
+    correct_path = input_path[:-8] + ".md"
+    year, month, day, name = splitInfo(correct_path)
+    base = getBase(year, month, day, name, part)
 
     formatted_title = []
     links = []
@@ -241,51 +242,51 @@ for fname in files:
     format_mark = []
     levels = []
     for item in headers:
+        # 获取标题对应的id
         lev, show, res = getTitle(item)
         formatted_title.append(res)
+        # 由每一篇博客的网址和标题id信息拼接url
         link = getLink(base, res)
         links.append(link)
         levels.append(lev)
+        # 拼接Markdown格式的超链接
         content = "[" + show + "](" + link + ")"
         bookmarks.append(content)
 
+    # 寻找标题最大等级(数字最小)，以此作为一级列表
+    # 因为在博客中很多都是直接从3级甚至4级标题开始的
+    # 因此没必要空出来1、2级标题的层次，非常难看，直接把3或4当作第一级
     min_level = (min(levels))
     for i in range(levels.__len__()):
         levels[i] = levels[i] - min_level
 
+    # 基于标题不同等级，按照Markdown语法生成TOC
     for i in range(bookmarks.__len__()):
         format_mark.append(generateTOC(levels[i], bookmarks[i]))
 
     new_lines = []
-    new_lines.append(lines[0])
-    new_lines.append(lines[1])
-    new_lines.append(lines[2])
-    new_lines.append(lines[3])
-    new_lines.append(lines[4])
-    new_lines.append(lines[5])
-    new_lines.append(lines[6])
-    new_lines.append(lines[7])
-    new_lines.append(lines[8])
-    new_lines.append(lines[9])
-    new_lines.append(lines[10])
-    new_lines.append(lines[11])
-
-    new_lines.append("#### Content\n")
-
+    # 按照我自己的post格式，将TOC插入在前12行之后
+    for i in range(12):
+        new_lines.append(lines[i])
+    # 写入TOC
     for item in format_mark:
         new_lines.append(item + "\n")
-
-    new_lines.append("<hr>\n")
-
+    # 添加TOC与正文之间的分隔线
+    new_lines.append("<hr style=\"margin:0em 0em 1.75em 0em;\">\n")
+    # 写入正文剩余部分内容
     for i in range(11, lines.__len__()):
         new_lines.append(lines[i])
 
-    for item in new_lines:
-        print(item)
-
+    # 将重新生成的post内容输出到md文件中，覆盖原文件
     out_toc = ""
     for item in new_lines:
         out_toc = out_toc + item
+    path = correct_path[:-2] + "_toc.md"
     fout = open(path.decode('utf8'), 'w')
     fout.writelines(out_toc)
     fout.close()
+
+    print("Success!")
+
+    input_path = ""
+    execFunction(input_path)
